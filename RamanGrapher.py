1cb9fab9489b3aa49735f5aa97aba8db06ae9181
Marc-Andre <marc-andre.vigneault.02@hotmail.com>
2021-01-22 16:52:03 -0500

    update requirements.txt

--- RamanGrapher.py
+++ RamanGrapher.py
@@ -1,5 +1,4 @@
 from __future__ import annotations
-import numpy as np
 from matplotlib import pyplot as plt
 from tkinter.filedialog import asksaveasfilename
 from scipy import signal
--- main.py
+++ main.py
@@ -7,18 +7,18 @@ rmg1.load_calibration_polynomial(8.66*10**-9, -3.51*10**-5, 0.156, 799, unit="nm
 rmg1.load_curvature_polynomial(0.000125, -0.05, +5)
 
 # 1- Create Images Objects
-test = RamanSpectrumImage(".\\data\ \25-09-2020\\test.tif")
-test2 = RamanSpectrumImage(".\\data\\25-09-2020\\test.tif")
+test = RamanSpectrumImage(".\\data\\surface\\25-09-2020\\test.tif")
+test2 = RamanSpectrumImage(".\\data\\surface\\25-09-2020\\test.tif")
 
 # 2- Make operation on Image Objects
-result = test-test2
+#result = test-test2
 
 # 3- Load Images Object inside RamanGrapher (rmg1)
-rmg1.load_ramanSpectrum_image(result)
+rmg1.load_ramanSpectrum_image(test)
 
 # 4- Add_plot or/and add_image and then show()
 # rmg1.add_image()
-rmg1.add_plot(figsize=(6, 4), xunit='cm-1', xlimits=(2, 1340), ylimits=(20, 400),  normalized=False, uncurved=True, label=", Vodka [40% eth] on SERS , 5min, 31mW", majorTicks="auto", minorTicks="auto")
+rmg1.add_plot(figsize=(6, 4), xunit='cm-1', xlimits=(2, 1340), ylimits=(20, 400),  normalized=False, uncurved=True, label=", Vodka [40% eth] on SERS , 5min, 31mW", majorTicks=250, minorTicks="auto")
 rmg1.show()
 
 '''TODO: normalization when zero crash'''
