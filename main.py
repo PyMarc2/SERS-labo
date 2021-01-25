3c7842a4adf475bee0e4130166bbae92cb4ce321
Marc-Andre <marc-andre.vigneault.02@hotmail.com>
2021-01-25 14:39:51 -0500

    new polystyrene spectrum ahah

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ rmg1.load_calibration_polynomial(8.66*10**-9, -3.51*10**-5, 0.156, 799, unit="nm
 rmg1.load_curvature_polynomial(0.000125, -0.05, +5)
 
 # 1- Create Images Objects
-test = RamanSpectrumImage(".\\data\\surface\\25-09-2020\\test.tif")
-test2 = RamanSpectrumImage(".\\data\\surface\\25-09-2020\\test.tif")
+test = RamanSpectrumImage(".\\data\\volumique\\23-01-2021\\glutamate1.tif")
 
 # 2- Make operation on Image Objects
 #result = test-test2
@@ -18,7 +17,9 @@ rmg1.load_ramanSpectrum_image(test)
 
 # 4- Add_plot or/and add_image and then show()
 # rmg1.add_image()
-rmg1.add_plot(figsize=(6, 4), xunit='cm-1', xlimits=(2, 1340), ylimits=(20, 400),  normalized=False, uncurved=True, label=", Vodka [40% eth] on SERS , 5min, 31mW", majorTicks=250, minorTicks="auto")
+rmg1.add_plot(figsize=(6, 4), xunit='cm-1', xlimits=(2, 940), ylimits=(20, 400),  normalized=False, uncurved=True, label="Polystyrene Raman\nSpectrum", majorTicks=250, minorTicks="auto")
+
 rmg1.show()
+
 
 '''TODO: normalization when zero crash'''
